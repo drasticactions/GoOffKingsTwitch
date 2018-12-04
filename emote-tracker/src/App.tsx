82cb5c58b3a404e0f0fc83d55350c7c632a7ce2f
Tim Miller <innerlogic4321@gmail.com>
2018-12-04 17:32:14 -0500

    Start tracking emotes

--- emote-tracker/src/App.tsx
+++ emote-tracker/src/App.tsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import { Tokens } from './tokens';
 import Twitch from 'twitch-js';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+import { find } from 'lodash';
 
+class EmoteCount {
+  id: number;
+  @observable count: number;
+}
+
+@observer
 class App extends Component<any, any> {
   tokens: Tokens = new Tokens();
   client: any;
   setup: boolean = false;
+  @observable emoteCounts: EmoteCount[] = [];
+
   constructor(props) {
     super(props);
     this.setup = this.tokens.TWITCH_TOKEN != "" && this.tokens.TWITCH_USERNAME != "";
@@ -16,7 +27,7 @@ class App extends Component<any, any> {
   }
 
   async setupClient() {
-    const { api, chat, chatConstants } = new Twitch({token: this.tokens.TWITCH_TOKEN, username: this.tokens.TWITCH_USERNAME})
+    const { api, chat, chatConstants } = new Twitch({ token: this.tokens.TWITCH_TOKEN, username: this.tokens.TWITCH_USERNAME })
     console.log(chatConstants);
     chat.on(chatConstants.EVENTS.PRIVATE_MESSAGE, (msg) => this.logEvent(msg));
     await chat.connect();
@@ -25,8 +36,23 @@ class App extends Component<any, any> {
   }
 
   logEvent(msg) {
-    console.log(msg);
+    if (msg.tags == null || msg.tags.emotes == null)
+      return;
+    for (let emote of msg.tags.emotes) {
+      let emoteTag = find(this.emoteCounts, { id: emote.id })
+      if (emoteTag == null) {
+        this.emoteCounts.push({
+          id: emote.id,
+          count: 1
+        });
+      }
+      else {
+        emoteTag.count = emoteTag.count + 1;
+      }
+    }
   }
+
+
 
   renderMissingTokens() {
     return (
@@ -41,7 +67,7 @@ class App extends Component<any, any> {
       return this.renderMissingTokens();
     return (
       <div className="App">
-        
+        {this.emoteCounts.map(u => <p>{`${u.id} - ${u.count}`}</p>)}
       </div>
     );
   }
