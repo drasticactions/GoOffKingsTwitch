677208242470e4ec68267922cce996838989d3d4
Tim Miller <innerlogic4321@gmail.com>
2019-01-24 21:38:01 -0500

    Update jf-stefan-wins

--- jf-stefan-wins/src/App.tsx
+++ jf-stefan-wins/src/App.tsx
@@ -8,6 +8,49 @@ import posed, { PoseGroup } from 'react-pose';
 import { easing } from 'popmotion'
 
 
+const Rob = posed.div({
+  preEnter: {
+    opacity: 0
+  },
+  enter: {
+    x: 15,
+    opacity: 1,
+    delayChildren: 1200,
+    staggerChildren: 80,
+    transition: {
+      opacity: {
+        duration: 600,
+        ease: easing.linear
+      },
+      x: { type: 'spring' }
+    }
+  },
+  GoOff: {
+    scale: ({ dynScale }) => dynScale,
+    transition: ({ dynStiff }) => ({
+      type: 'spring',
+      stiffness: dynStiff,
+      damping: 0
+    }),
+    props: { dynStiff: 200, dynScale: 1 }
+  },
+  none: {
+    scale: 1
+  },
+  exit: {
+    opacity: 0,
+    delay: 800,
+    staggerChildren: 50,
+    transition: {
+      opacity: {
+        duration: 600,
+        ease: easing.linear
+      },
+      x: { type: 'spring' }
+    }
+  }
+});
+
 const Stefan = posed.div({
   preEnter: {
     x: -250,
@@ -104,6 +147,7 @@ class App extends Component<any, any> {
   setup: boolean = false;
   @observable jfEmoteCount: number = 0;
   @observable stefanEmoteCount: number = 0;
+  @observable robEmoteCount: number = 0;
   constructor(props) {
     super(props);
     this.setup = this.tokens.TWITCH_TOKEN != "" && this.tokens.TWITCH_USERNAME != "";
@@ -125,6 +169,7 @@ class App extends Component<any, any> {
   coolDown() {
     this.jfEmoteCount = this.jfEmoteCount < 1 ? 0 : this.jfEmoteCount * .9;
     this.stefanEmoteCount = this.stefanEmoteCount < 1 ? 0 : this.stefanEmoteCount * .9;
+    this.robEmoteCount = this.robEmoteCount < 1 ? 0 : this.robEmoteCount * .9;
   }
 
   logEvent(msg) {
@@ -132,6 +177,7 @@ class App extends Component<any, any> {
       return;
     let jfEmoteCount = 0;
     let stefanEmoteCount = 0;
+    let robEmoteCount = 0;
     for (let emote of msg.tags.emotes) {
       let emoteString = msg.message.substring(emote.start, emote.end + 1);
       console.log(emoteString);
@@ -139,9 +185,12 @@ class App extends Component<any, any> {
         jfEmoteCount = jfEmoteCount + 1;
       else if (emoteString == "stefan13StefanWin")
         stefanEmoteCount = stefanEmoteCount + 1;
+      else if (emoteString == "stefan13Robwhat")
+      robEmoteCount = robEmoteCount + 1;
     }
     this.jfEmoteCount = this.jfEmoteCount + jfEmoteCount;
     this.stefanEmoteCount = this.stefanEmoteCount + stefanEmoteCount;
+    this.robEmoteCount = this.robEmoteCount + robEmoteCount;
   }
 
   renderMissingTokens() {
@@ -150,6 +199,15 @@ class App extends Component<any, any> {
         <h2>You must add your Twitch tokens to tokens.ts!</h2>
       </div>
     );
+  }
+
+  renderRob() {
+    let scaleCount = 1 + (this.robEmoteCount * .01);
+    if (scaleCount > 2.5)
+      scaleCount = 2.5;
+    let dynStiff = 200 * scaleCount;
+    return <Rob initialPose="none" pose={this.robEmoteCount > 0 ? "GoOff" : "none"} key="rob" poseKey={this.robEmoteCount}
+      dynScale={scaleCount <= 1 ? 1 : scaleCount} dynStiff={dynStiff} className="rob" />
   }
 
   renderStefan() {
@@ -178,6 +236,7 @@ class App extends Component<any, any> {
       <PoseGroup preEnterPose="preEnter">
         {this.stefanEmoteCount > 3 && this.renderStefan()}
         {this.jfEmoteCount > 3 && this.renderJF()}
+        {this.robEmoteCount > 3 && this.renderRob()}
       </PoseGroup>
       </div>
     );
